505df5d7023776df2dce7c2f0c405b6ae810170b
André Santos <andre.orbe@gmail.com>
2025-05-29 19:39:40 -0300

    Removing debug mode

--- clp.py
+++ clp.py
@@ -1,12 +1,11 @@
 from pycomm3 import LogixDriver
-from pycomm3.logger import configure_default_logger, LOG_VERBOSE, LOG_DEBUG
-
+from pycomm3.logger import configure_default_logger, LOG_VERBOSE
 
 class CLP:
 
-    def init(self, ip_address, log_file="rockwell.log"):
+    def __init__(self, ip_address, log_file="rockwell.log"):
         self.plc = LogixDriver(ip_address)
-        configure_default_logger(level=LOG_DEBUG, filename=log_file)
+        # configure_default_logger(level=LOG_VERBOSE, filename=log_file)
         
     def connect(self):
         self.plc.open()
